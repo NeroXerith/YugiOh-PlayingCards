0e2067aa82ded70af02af9deafa64e08e29f5a10
NeroXerith <astar8820@gmail.com>
2025-03-13 21:16:20 +0800

    feat(HomeViewModel): Utilize Combine concepts to handle API Responses

--- YugiOh-PlayingCards/View Controllers/Home/HomeViewModel.swift
+++ YugiOh-PlayingCards/View Controllers/Home/HomeViewModel.swift
@@ -5,103 +5,131 @@
 //  Created by Biene Bryle Sanico on 3/12/25.
 //
 import Foundation
+import Combine
 
-enum Types: Int, CaseIterable {
-    case all
-    case monsters
-    case spell
-    case trap
-    
-    var name: String {
-        switch self {
-        case .all: return "All"
-        case .monsters: return "Monsters"
-        case .spell: return "Spell"
-        case .trap: return "Trap"
-        }
-    }
-}
+//enum Types: Int, CaseIterable {
+//    case all
+//    case monsters
+//    case spell
+//    case trap
+//    
+//    var name: String {
+//        switch self {
+//        case .all: return "All"
+//        case .monsters: return "Monsters"
+//        case .spell: return "Spell"
+//        case .trap: return "Trap"
+//        }
+//    }
+//}
 
 final class HomeViewModel {
-    weak var delegate: RequestDelegate?
-    private var state: ViewState {
-        didSet {
-            self.delegate?.didUpdate(with: state)
-        }
-    }
+//    weak var delegate: RequestDelegate?
+//    private var state: ViewState {
+//        didSet {
+//            self.delegate?.didUpdate(with: state)
+//        }
+//    }
     private var cardFetcher = YgorprodeckService()
-    private var cards: [Card] = []
-    private var filteredCards: [Card] = []
-    private var selectedType: Types = .all {
-        didSet {
-            self.filterData()
-        }
+    private var cancellables = Set<AnyCancellable>()
+//    private var cards: [Card] = []
+//    private var filteredCards: [Card] = []
+//    private var selectedType: Types = .all {
+//        didSet {
+//            self.filterData()
+//        }
+//    }
+//    
+//    init() {
+//        self.state = .idle
+//    }
+    
+    init(cardFetcher: YgorprodeckService){
+        self.cardFetcher = cardFetcher
     }
     
-    init() {
-        self.state = .idle
-    }
-}
-
-// MARK: - Data Source
-extension HomeViewModel {
-    var numberOfItems: Int {
-        filteredCards.count
-    }
+    @Published private(set) var state: ViewState = .idle
+    @Published private(set) var items: [Card] = []
     
-    func getInfo(for indexPath: IndexPath) -> (name: String, type: String, desc: String, imageURL: String?) {
-        let card = filteredCards[indexPath.row]
-        return (name: card.name, type: card.type, desc: card.desc, imageURL: card.cardImages.first?.imageURL)
-    }
-}
-
-// MARK: - Service
-extension HomeViewModel {
     func loadData() {
-        self.state = .loading
-        cardFetcher.fetchAllCards { [weak self] result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let cards):
-                    self?.cards = cards
-                    self?.filteredCards = cards
-                    self?.state = .success
-                case .failure(let error):
-                    self?.cards = []
-                    self?.filteredCards = []
-                    self?.state = .error(error)
-                }
-            }
-        }
-    }
-    
-    func filterByType(type: Types) {
-        self.selectedType = type
-    }
-    
-    func selectedTypeName() -> String {
-        self.selectedType.name
-    }
-}
-
-private extension HomeViewModel {
-    func filterData() {
-        guard selectedType != .all else {
-            self.filteredCards = cards
-            self.state = .success
-            return
-        }
+        state = .loading
         
-        guard selectedType != .monsters else {
-            self.filteredCards = self.cards.filter {
-                !$0.type.lowercased().contains(Types.spell.name.lowercased()) && !$0.type.lowercased().contains(Types.trap.name.lowercased()) }
-            self.state = .success
-            return
-            
-        }
-        self.filteredCards = self.cards.filter {
-            $0.type.lowercased().contains(self.selectedType.name.lowercased())
-        }
-            self.state = .success
+        cardFetcher.fetchAllCards()
+            .receive(on: DispatchQueue.main) // Main thread UI Updates
+            .sink(receiveCompletion: { [weak self] response in
+                guard let self else { return }
+                switch response {
+                case .finished:
+                    self.state = .success
+                case .failure(let error):
+                    self.state = .error(error)
+                }
+            }, receiveValue: { [weak self] cards in
+                self?.items = cards
+            })
+            .store(in: &cancellables)
     }
 }
+//
+//// MARK: - Data Source
+//extension HomeViewModel {
+//    var numberOfItems: Int {
+//        filteredCards.count
+//    }
+//    
+//    func getInfo(for indexPath: IndexPath) -> (name: String, type: String, desc: String, imageURL: String?) {
+//        let card = filteredCards[indexPath.row]
+//        return (name: card.name, type: card.type, desc: card.desc, imageURL: card.cardImages.first?.imageURL)
+//    }
+//}
+//
+//// MARK: - Service
+//extension HomeViewModel {
+//    func loadData() {
+//        self.state = .loading
+//        cardFetcher.fetchAllCards { [weak self] result in
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .success(let cards):
+//                    self?.cards = cards
+//                    self?.filteredCards = cards
+//                    self?.state = .success
+//                case .failure(let error):
+//                    self?.cards = []
+//                    self?.filteredCards = []
+//                    self?.state = .error(error)
+//                }
+//            }
+//        }
+//    }
+//    
+//    func filterByType(type: Types) {
+//        self.selectedType = type
+//    }
+//    
+//    func selectedTypeName() -> String {
+//        self.selectedType.name
+//    }
+//}
+//
+//private extension HomeViewModel {
+//    func filterData() {
+//        guard selectedType != .all else {
+//            self.filteredCards = cards
+//            self.state = .success
+//            return
+//        }
+//        
+//        guard selectedType != .monsters else {
+//            self.filteredCards = self.cards.filter {
+//                !$0.type.lowercased().contains(Types.spell.name.lowercased()) && !$0.type.lowercased().contains(Types.trap.name.lowercased()) }
+//            self.state = .success
+//            return
+//            
+//        }
+//        self.filteredCards = self.cards.filter {
+//            $0.type.lowercased().contains(self.selectedType.name.lowercased())
+//        }
+//            self.state = .success
+//    }
+//}
